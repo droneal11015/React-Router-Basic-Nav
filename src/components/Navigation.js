a733bab1d2654d593eb621a317cc021ddd606341
droneal11015 <droneal11015@gmail.com>
2019-05-13 18:43:29 -0400

    added links to nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <div>
-      <div className="App">
+      <div className='App'>
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+          <Link to='/'>
+            <a href=''>Home</a>
+          </Link>
         </div>
         <div>
-          <a href="">About</a>
+          <Link to='/About'>
+            <a href=''>About</a>
+          </Link>
         </div>
         <div>
-          <a href="">Contact</a>
+          <Link to='/Contact'>
+            <a href=''>Contact</a>
+          </Link>
         </div>
       </div>
     </div>
